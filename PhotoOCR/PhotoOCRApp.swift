5333150a1c2daf40e60167b36edfbfde435b3847
ConC <Con.CCC@Gmail.com>
2024-11-24 17:18:47 +1100

    Initial Commit

---
+++ PhotoOCR/PhotoOCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoOCRApp.swift
+//  PhotoOCR
+//
+//  Created by Con Coucoumakis on 24/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoOCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
